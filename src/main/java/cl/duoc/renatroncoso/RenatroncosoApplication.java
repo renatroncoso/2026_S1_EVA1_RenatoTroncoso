c756976c97a37365f77766a5ede36c5795831b1e
rena <rena.troncoso@duocuc.cl>
2026-03-31 10:14:57 -0300

    commit inicial

---
+++ src/main/java/cl/duoc/renatroncoso/RenatroncosoApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.renatroncoso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenatroncosoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenatroncosoApplication.class, args);
+	}
+
+}

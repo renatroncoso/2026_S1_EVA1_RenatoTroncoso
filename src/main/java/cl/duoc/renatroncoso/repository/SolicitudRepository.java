180672e1bc6dc6e3e340aa5da24d709e1af3ab5e
rena <rena.troncoso@duocuc.cl>
2026-03-31 10:40:12 -0300

    commit Repository

--- src/main/java/cl/duoc/renatroncoso/repository/SolicitudRepository.java
+++ src/main/java/cl/duoc/renatroncoso/repository/SolicitudRepository.java
@@ -1,7 +1,11 @@
 package cl.duoc.renatroncoso.repository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+
 import cl.duoc.renatroncoso.model.Solicitud;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +13,21 @@ import org.springframework.stereotype.Repository;
 public class SolicitudRepository {
     private final Map<Long, Solicitud> solicitudes = new HashMap<>();
     
+    public List<Solicitud> findAll(){
+        return new ArrayList<>(solicitudes.values());
+    }
+    public Optional<Solicitud> findById(Long id){
+        return Optional.ofNullable(solicitudes.get(id));
+    }
+    public Solicitud save(Solicitud solicitud){
+        solicitudes.put(solicitud.getId(), solicitud);
+        return solicitud;
+    }
+    public void deleteById(Long id){
+        solicitudes.remove(id);
+    }
+    public boolean existsById(Long id){
+        return solicitudes.containsKey(id);
+    }
+
 }
